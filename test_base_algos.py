cf83d5ac07282375f0a9730f2cbc732d5aab82bc
Cody Wild <codywild@berkeley.edu>
2020-07-13 15:11:43 -0700

    Create test to quickly test functionality of all algorithms

--- test_base_algos.py
+++ test_base_algos.py
@@ -1,7 +1,29 @@
+import warnings
+
+# numpy warnings because of tensorflow
+warnings.filterwarnings("ignore", category=FutureWarning)
+from tensorflow.python.util import deprecation
+deprecation._PRINT_DEPRECATION_WARNINGS = False
+
 from algos import *
+from run_bc import represent_ex
+from sacred.observers import FileStorageObserver
+import pytest
 
-ALGOS_LIST = [el for el in globals() if issubclass(el, RepresentationLearner)]
+represent_ex.observers.append(FileStorageObserver('test_observer'))
 
 
-if __name__ == "__main__":
-    print(ALGOS_LIST)
+def is_representation_learner(el):
+    try:
+        return issubclass(el, RepresentationLearner) and el not in (RepresentationLearner, DynamicsPrediction, InverseDynamicsPrediction)
+    except TypeError:
+        return False
+
+
+@pytest.mark.parametrize("algo", [el for el in globals().values() if is_representation_learner(el)])
+def test_algo(algo):
+    print()
+    print()
+    print(f"Testing: {algo}")
+    represent_ex.run(config_updates={'pretrain_epochs': 1, 'timesteps': 500,
+                                     'train_from_expert': False, 'algo': algo})
