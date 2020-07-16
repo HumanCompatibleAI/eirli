11e10b28a7dc8612cb4b5516cafe0606f71405d6
Cody Wild <codywild@berkeley.edu>
2020-07-16 15:37:34 -0700

    Remove rl_baselines_zoo dependency

--- algos/utils.py
+++ algos/utils.py
@@ -11,7 +11,9 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from PIL import Image
 import cv2
-
+from stable_baselines3.common.vec_env import VecNormalize, VecFrameStack, DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.cmd_util import make_atari_env
 now = datetime.now()
 
 
@@ -157,3 +159,105 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
+
+
+def make_env(env_id, rank=0, seed=0, log_dir=None, wrapper_class=None, env_kwargs=None):
+    """
+    Helper function to multiprocess training
+    and log the progress.
+
+    :param env_id: (str)
+    :param rank: (int)
+    :param seed: (int)
+    :param log_dir: (str)
+    :param wrapper: (type) a subclass of gym.Wrapper to wrap the original
+                    env with
+    :param env_kwargs: (Dict[str, Any]) Optional keyword argument to pass to the env constructor
+    """
+    if log_dir is not None:
+        os.makedirs(log_dir, exist_ok=True)
+
+    if env_kwargs is None:
+        env_kwargs = {}
+
+    def _init():
+        set_global_seeds(seed + rank)
+        env = gym.make(env_id, **env_kwargs)
+
+        # Dict observation space is currently not supported.
+        # https://github.com/hill-a/stable-baselines/issues/321
+        # We allow a Gym env wrapper (a subclass of gym.Wrapper)
+        if wrapper_class:
+            env = wrapper_class(env)
+
+        env.seed(seed + rank)
+        log_file = os.path.join(log_dir, str(rank)) if log_dir is not None else None
+        env = Monitor(env, log_file)
+        return env
+
+    return _init
+
+
+def create_test_env(env_id, n_envs=1, is_atari=False,
+                    stats_path=None, seed=0,
+                    log_dir='', should_render=True, hyperparams=None, env_kwargs=None):
+    """
+    Create environment for testing a trained agent
+
+    :param env_id: (str)
+    :param n_envs: (int) number of processes
+    :param is_atari: (bool)
+    :param stats_path: (str) path to folder containing saved running averaged
+    :param seed: (int) Seed for random number generator
+    :param log_dir: (str) Where to log rewards
+    :param should_render: (bool) For Pybullet env, display the GUI
+    :param env_wrapper: (type) A subclass of gym.Wrapper to wrap the original
+                        env with
+    :param hyperparams: (dict) Additional hyperparams (ex: n_stack)
+    :param env_kwargs: (Dict[str, Any]) Optional keyword argument to pass to the env constructor
+    :return: (gym.Env)
+    """
+
+    if hyperparams is None:
+        hyperparams = {}
+
+    if env_kwargs is None:
+        env_kwargs = {}
+
+    if is_atari:
+        print("Using Atari wrapper")
+        env = make_atari_env(env_id, n_envs=n_envs, seed=seed)
+        # Frame-stacking with 4 frames
+        env = VecFrameStack(env, n_stack=4)
+    elif n_envs > 1:
+        # start_method = 'spawn' for thread safe
+        env = SubprocVecEnv([make_env(env_id, i, seed, log_dir, wrapper_class=None, env_kwargs=env_kwargs) for i in range(n_envs)])
+    # Pybullet envs does not follow gym.render() interface
+    elif "Bullet" in env_id:
+        # HACK: force SubprocVecEnv for Bullet env
+        env = SubprocVecEnv([make_env(env_id, 0, seed, log_dir, wrapper_class=None, env_kwargs=env_kwargs)])
+    else:
+        env = DummyVecEnv([make_env(env_id, 0, seed, log_dir, wrapper_class=None, env_kwargs=env_kwargs)])
+
+    # Load saved stats for normalizing input and rewards
+    # And optionally stack frames
+    if stats_path is not None:
+        if hyperparams.get('normalize'):
+            print("Loading running average")
+            print("with params: {}".format(hyperparams['normalize_kwargs']))
+            env = VecNormalize(env, training=False, **hyperparams['normalize_kwargs'])
+
+            if os.path.exists(os.path.join(stats_path, 'vecnormalize.pkl')):
+                env = VecNormalize.load(os.path.join(stats_path, 'vecnormalize.pkl'), env)
+                # Deactivate training and reward normalization
+                env.training = False
+                env.norm_reward = False
+            else:
+                # Legacy:
+                env.load_running_average(stats_path)
+
+        n_stack = hyperparams.get('frame_stack', 0)
+        if n_stack > 0:
+            print("Stacking {} frames".format(n_stack))
+            env = VecFrameStack(env, n_stack)
+    return env
