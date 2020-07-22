4d3ff231af928c105187878bce9037ea84471234
Sam Toyer <sam@qxcv.net>
2020-07-21 22:26:30 -0700

    It trains!

--- src/il_representations/envs/config.py
+++ src/il_representations/envs/config.py
@@ -6,15 +6,22 @@ benchmark_ingredient = Ingredient('benchmark')
 
 
 @benchmark_ingredient.config
-def bench_default():
-    benchmark_name = 'Benchmark not configured'
-
-
-@benchmark_ingredient.named_config
-def bench_magical():
+def bench_defaults():
     benchmark_name = 'magical'
+
+    # magical config variables
     magical_demo_dirs = {
-        # these defaults work well if you set up symlinks
+        # These defaults work well if you set up symlinks.
+        # For example, on perceptron/svm, you can set up the appropriate
+        # symlink structure with a script like this:
+        #
+        # mkdir -p data/magical;
+        # for dname in move-to-corner match-regions make-line fix-colour \
+        #     find-dupe cluster-colour cluster-shape move-to-region;
+        # do
+        #     ln -s /scratch/sam/il-demos/magical/${dname}-2020-*/ \
+        #         ./data/magical/${dname};
+        # done
         'MoveToCorner': 'data/magical/move-to-corner/',
         'MoveToRegion': 'data/magical/move-to-region/',
         'MatchRegions': 'data/magical/match-regions/',
@@ -25,10 +32,6 @@ def bench_magical():
         'ClusterShape': 'data/magical/cluster-shape/',
 
     }
-    magical_env_name = 'MoveToCorner'
+    magical_env_prefix = 'MoveToCorner'
 
-
-@benchmark_ingredient.named_config
-def bench_dm_control():
-    # FIXME(sam): implement all of this
-    benchmark_name = 'dm_control'
+    # TODO(sam): add dm_control support as well
