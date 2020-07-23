e238eda02987c55f6195b30d877b3c75d8a665bd
Cody Wild <codywild@berkeley.edu>
2020-07-23 15:00:44 -0700

    Add flag for finetuning, and simplify SimplePolicyHead

--- policy_interfacing.py
+++ policy_interfacing.py
@@ -3,7 +3,7 @@ from stable_baselines3.common.policies import BaseFeaturesExtractor
 
 
 class EncoderFeatureExtractor(BaseFeaturesExtractor):
-    def __init__(self, observation_space, features_dim, encoder=None, encoder_path=None):
+    def __init__(self, observation_space, features_dim, encoder=None, encoder_path=None, finetune=True):
         super().__init__(observation_space, features_dim)
         assert encoder is not None or encoder_path is not None, "You must pass in either an encoder object or a path to an encoder"
         assert not (encoder is not None and encoder_path is not None), "Please pass in only one of `encoder` and `encoder_path`"
@@ -12,28 +12,23 @@ class EncoderFeatureExtractor(BaseFeaturesExtractor):
         else:
             self.representation_encoder = torch.load(encoder_path)
 
+        if not finetune:
+            for param in self.representation_encoder.parameters():
+                param.requires_grad = False
+
     def forward(self, observations):
         features_dist = self.representation_encoder(observations, traj_info=None)
         return features_dist.loc
 
 
-class EncoderPolicyHead(torch.nn.Module):
-    def __init__(self, encoder, action_size, device, trained_weights_path):
-        super().__init__()
-        self.representation_encoder = encoder
-        self.trained_weights_path = trained_weights_path
-        if self.trained_weights_path is not None:
-            self._load_encoder_weights()
-        self.device = device
+class EncoderSimplePolicyHead(EncoderFeatureExtractor):
+    # Not actually a FeatureExtractor for SB use, but a very simple Policy for use in Cynthia's BC code
+    def __init__(self, observation_space, features_dim, action_size, encoder=None, encoder_path=None, finetune=True):
+        super().__init__(observation_space, features_dim, encoder, encoder_path, finetune)
         self.action_layer = torch.nn.Linear(encoder.representation_dim, action_size)
         self.softmax = torch.nn.Softmax(dim=-1)
 
-    def _load_encoder_weights(self):
-        pretrained_dict = torch.load(self.trained_weights_path, device=self.device)
-        self.representation_encoder.load_state_dict(pretrained_dict, map_location=self.device)
-
-    def forward(self, x):
-        representation_dist = self.representation_encoder(x)
-        representation = representation_dist.loc
+    def forward(self, observations):
+        representation = super().forward(observations)
         action_probas = self.softmax(self.action_layer(representation))
         return action_probas
