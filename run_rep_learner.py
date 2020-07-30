ce93180f0c8e12b240cfaa8c920337821e3be5e6
Cody Wild <codywild@berkeley.edu>
2020-07-30 14:06:44 -0700

    Working but slow CEB

--- run_rep_learner.py
+++ run_rep_learner.py
@@ -17,28 +17,53 @@ import inspect
 represent_ex = Experiment('representation_learning')
 
 
+
 @represent_ex.config
 def default_config():
     env_id = 'BreakoutNoFrameskip-v4'
     algo = "SimCLR"
     n_envs = 1
     train_from_expert = True
-    timesteps = 640
+    demo_timesteps = 640
+    rl_training_timesteps = 1000
     pretrain_only = False
     pretrain_epochs = 50
     representation_dim = 128
     ppo_finetune = True
-    learn_from_expert = False
-    rollouts_path = f"{env_id}_rollouts_500_ts_100_traj.npy"
     _ = locals()
     del _
 
 
-def get_random_trajectories(env, timesteps):
+@represent_ex.named_config
+def ceb_long_breakout():
+    env_id = 'BreakoutNoFrameskip-v4'
+    train_from_expert = True
+    algo = algos.CEB
+    #loss_constructor_kwargs = {'beta': 0.1}
+    pretrain_epochs = 50
+    demo_timesteps = None
+    ppo_finetune=False
+    _ = locals()
+    del _
+
+@represent_ex.named_config
+def ceb_no_compression_long_breakout():
+    env_id = 'BreakoutNoFrameskip-v4'
+    train_from_expert = True
+    algo = algos.CEB
+    loss_constructor_kwargs = {'beta': 0.0}
+    pretrain_epochs = 50
+    demo_timesteps = None
+    ppo_finetune=False
+    _ = locals()
+    del _
+
+@represent_ex.capture
+def get_random_trajectories(env, demo_timesteps):
     # Currently not designed for VecEnvs with n>1
     trajectory = {'states': [], 'actions': [], 'dones': []}
     obs = env.reset()
-    for i in range(timesteps):
+    for i in range(demo_timesteps):
         trajectory['states'].append(obs.squeeze())
         action = np.array([env.action_space.sample() for _ in range(env.num_envs)])
         obs, rew, dones, info = env.step(action)
@@ -47,8 +72,9 @@ def get_random_trajectories(env, timesteps):
     return trajectory
 
 @represent_ex.capture
-def get_expert_trajectories(env_id, timesteps, rollouts_path):
+def get_expert_trajectories(env_id, demo_timesteps):
     expert_data_loc = "/Users/cody/Data/expert_rollouts/"
+    rollouts_path = f"{env_id}_rollouts_500_ts_100_traj.npy"
     full_rollouts_path = os.path.join(expert_data_loc, rollouts_path)
     trajectories = np.load(full_rollouts_path, allow_pickle=True)
     merged_trajectories = {'states': [], 'actions': [], 'dones': []}
@@ -56,12 +82,12 @@ def get_expert_trajectories(env_id, timesteps, rollouts_path):
     for ind, traj in enumerate(trajectories):
         for k in merged_trajectories.keys():
             merged_trajectories[k] += traj[k]
-        if len(merged_trajectories['states']) > timesteps:
+        if demo_timesteps is not None and len(merged_trajectories['states']) > demo_timesteps:
             for k in merged_trajectories.keys():
-                merged_trajectories[k] = merged_trajectories[k][0:timesteps]
+                merged_trajectories[k] = merged_trajectories[k][0:demo_timesteps]
             break
-    if len(merged_trajectories['states']) < timesteps:
-        raise Warning("")
+    if demo_timesteps is not None and len(merged_trajectories['states']) < demo_timesteps:
+        raise Warning(f"Requested {demo_timesteps} timesteps, only was able to read in {len(merged_trajectories['states'])}")
     return merged_trajectories
 
 
@@ -76,7 +102,7 @@ def initialize_non_features_extractor(sb3_model):
 
 
 @represent_ex.main
-def run(env_id, seed, algo, n_envs, timesteps, representation_dim, ppo_finetune, learn_from_expert, _config):
+def run(env_id, seed, algo, n_envs, pretrain_epochs, rl_training_timesteps, representation_dim, ppo_finetune, train_from_expert, _config):
 
     # TODO fix to not assume FileStorageObserver always present
     log_dir = os.path.join(represent_ex.observers[0].dir, 'training_logs')
@@ -97,10 +123,10 @@ def run(env_id, seed, algo, n_envs, timesteps, representation_dim, ppo_finetune,
         env = VecFrameStack(make_atari_env(env_id, n_envs, seed), 4)
     else:
         env = gym.make(env_id)
-    if learn_from_expert:
-        data = get_expert_trajectories(env_id=env_id, timesteps=timesteps)
+    if train_from_expert:
+        data = get_expert_trajectories()
     else:
-        data = get_random_trajectories(env=env, timesteps=timesteps)
+        data = get_random_trajectories(env=env)
     assert issubclass(algo, RepresentationLearner)
 
     rep_learner_params = inspect.getfullargspec(RepresentationLearner.__init__).args
@@ -121,7 +147,7 @@ def run(env_id, seed, algo, n_envs, timesteps, representation_dim, ppo_finetune,
                          'ortho_init': False}
         ppo_model = PPO(policy=ActorCriticPolicy, env=env, verbose=1, policy_kwargs=policy_kwargs)
         ppo_model = initialize_non_features_extractor(ppo_model)
-        ppo_model.learn(total_timesteps=1000)
+        ppo_model.learn(total_timesteps=rl_training_timesteps)
         env.close()
 
 
