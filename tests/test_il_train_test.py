e0f1385e023170fdddcc28f512113ab5122c1a2f
Sam Toyer <sam@qxcv.net>
2020-08-04 15:29:07 -0700

    Smoke test

--- tests/test_il_train_test.py
+++ tests/test_il_train_test.py
@@ -1,31 +1,52 @@
 import glob
 import os
+
+import pytest
 from sacred.observers import FileStorageObserver
 
 from il_representations.scripts.il_test import il_test_ex
 from il_representations.scripts.il_train import il_train_ex
 
+# HACK(sam): this should be a 'run once' fixture
+observer = FileStorageObserver('test_observer')
+il_train_ex.observers.append(observer)
+il_test_ex.observers.append(observer)
 
-def test_il_train_test():
+@pytest.mark.parametrize("benchmark_name", ["magical", "dm_control"])
+def test_il_train_test(benchmark_name):
     """Simple smoke test for training/testing IL code."""
     # experiment config
-    observer = FileStorageObserver('test_observer')
-    il_train_ex.observers.append(observer)
-    il_test_ex.observers.append(observer)
 
     # benchmark config
     this_dir = os.path.dirname(os.path.abspath(__file__))
-    common_cfg = {
-        'dev_name': 'cpu',
-        'benchmark': {
-            'benchmark_name': 'magical',
-            'magical_env_prefix': 'MoveToRegion',
-            'magical_demo_dirs': {
-                'MoveToRegion':
-                os.path.join(this_dir, 'data', 'magical', 'move-to-region'),
+    if benchmark_name == 'magical':
+        common_cfg = {
+            'dev_name': 'cpu',
+            'benchmark': {
+                'benchmark_name': 'magical',
+                'magical_env_prefix': 'MoveToRegion',
+                'magical_demo_dirs': {
+                    'MoveToRegion':
+                    os.path.join(this_dir, 'data', 'magical',
+                                 'move-to-region'),
+                }
             }
         }
-    }
+    elif benchmark_name == 'dm_control':
+        common_cfg = {
+            'dev_name': 'cpu',
+            'benchmark': {
+                'benchmark_name': 'dm_control',
+                'dm_control_env': 'reacher-easy',
+                'dm_control_demo_patterns': {
+                    'reacher-easy':
+                    os.path.join(this_dir, 'data', 'dm_control',
+                                 'reacher-easy-*.pkl.gz'),
+                }
+            }
+        }
+    else:
+        raise NotImplementedError(f"How do I handle '{benchmark_name}'?")
 
     # train
     run_result = il_train_ex.run(config_updates={
@@ -38,9 +59,10 @@ def test_il_train_test():
 
     # test
     policy_path = glob.glob(os.path.join(log_dir, '*.pt'))[0]
-    il_test_ex.run(config_updates={
-        'n_rollouts': 2,
-        'eval_batch_size': 2,
-        'policy_path': policy_path,
-        **common_cfg,
-    })
+    il_test_ex.run(
+        config_updates={
+            'n_rollouts': 2,
+            'eval_batch_size': 2,
+            'policy_path': policy_path,
+            **common_cfg,
+        })
