2b9b74a15f4f2e300de3021c2198fd185c7d2638
Sam Toyer <sam@qxcv.net>
2020-08-07 16:56:35 -0700

    il_train.py now does encoder loading

--- src/il_representations/policy_interfacing.py
+++ src/il_representations/policy_interfacing.py
@@ -3,15 +3,28 @@ from stable_baselines3.common.policies import BaseFeaturesExtractor
 
 
 class EncoderFeatureExtractor(BaseFeaturesExtractor):
-    def __init__(self, observation_space, features_dim, encoder=None, encoder_path=None, finetune=True):
-        super().__init__(observation_space, features_dim)
+    def __init__(self, observation_space, features_dim=None, encoder=None, encoder_path=None, finetune=True):
         # Allow user to either pass in an existing encoder, or a path from which to load a pickled encoder
-        assert encoder is not None or encoder_path is not None, "You must pass in either an encoder object or a path to an encoder"
-        assert not (encoder is not None and encoder_path is not None), "Please pass in only one of `encoder` and `encoder_path`"
+        assert encoder is not None or encoder_path is not None, \
+            "You must pass in either an encoder object or a path to an encoder"
+        assert not (encoder is not None and encoder_path is not None), \
+            "Please pass in only one of `encoder` and `encoder_path`"
         if encoder is not None:
-            self.representation_encoder = encoder
+            representation_encoder = encoder
         else:
-            self.representation_encoder = torch.load(encoder_path)
+            representation_encoder = torch.load(encoder_path)
+
+        # do forward prop to infer the feature dim
+        if features_dim is None:
+            sample_obs = torch.FloatTensor(observation_space.sample()[None])
+            dev_encoder = encoder.to(sample_obs.device)
+            sample_dist = dev_encoder(sample_obs, traj_info=None)
+            sample_out, = sample_dist.sample()
+            features_dim, = sample_out.shape
+
+        super().__init__(observation_space, features_dim)
+
+        self.representation_encoder = representation_encoder
 
         if not finetune:
             # Set requires_grad to false if we want to not further train weights
