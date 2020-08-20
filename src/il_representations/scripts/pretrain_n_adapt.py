bb78a5cc1c6a9829971b4ea6a90d741fcc1c3a99
Cynthia Chen <RPC2@users.noreply.github.com>
2020-08-20 23:40:03 +0800

    basic structure

--- src/il_representations/scripts/pretrain_n_adapt.py
+++ src/il_representations/scripts/pretrain_n_adapt.py
@@ -2,54 +2,77 @@
 This file should support:
 1. Running representation pretrain for X seeds, then adapt on an IL algo for Y seeds.
 2. Grid search over different configurations
-
-Some design questions:
-1. Is it possible to collect the above runs in the same folder?
 """
-import itertools
+import os.path as osp
 from sacred import Experiment
-from il_representations.scripts.run_rep_laerner import represent_ex
+from sacred.observers import FileStorageObserver
+import ray
+from ray import tune
+from il_representations.scripts.run_rep_learner import represent_ex
 from il_representations.scripts.il_train import il_train_ex
+from utils import sacred_copy, update, detect_ec2
 
-pretrain_adapt_ex = Experiment('pretrain_n_adapt')
-
-
-@pretrain_adapt_ex.named_config
-def grid_search_config():
-    pretrain_params = {'seed': [1, 2, 3], 'lr': [0.1, 0.2]}
-    pretrain_configs = get_grid_combinations(pretrain_params)
-
-    adapt_params = {'seed': [4, 5, 6], 'lr': [0.01, 0.02]}
-    adapt_configs = get_grid_combinations(adapt_params)
+chain_ex = Experiment('chain', ingredients=[represent_ex, il_train_ex])
 
 
-@pretrain_adapt_ex.named_config
-def pretrain_n_adapt():
-    pretrain_configs = [{}]
-    adapt_config = [{}]
+def run_experiment(ex, ex_config, config, observer_name):
+    ex_dict = dict(ex_config)
+    merged_config = update(ex_dict, config)
+
+    observer = FileStorageObserver(osp.join(observer_name))
+    ex.observers.append(observer)
+    ret_val = ex.run(config_updates=merged_config)
+    ray.tune.track.log(accuracy=ret_val.result)
 
 
-def get_grid_combinations(configs):
-    """
-    This method will return a list of key-value combinations over a dictionary. It's useful for conducting grid search
-    over specified hyperparameters.
-    Example:
-        input: {1: [a, b], 2: [c, d]}
-        output: [{1: a, 2: c}, {1: a, 2: d}, {1: b, 2: c}, {1: b, 2: d}]
-    """
-    keys, values = zip(*configs.items())
-    config_grid = [dict(zip(keys, v)) for v in itertools.product(*values)]
-    return config_grid
+@chain_ex.config
+def base_config(representation_learning, il_train):
+    exp_name = "grid_search"
+    run_rep = True
+    run_il = True
+    spec = {
+        'rep': {
+            'algo': tune.grid_search(['MoCo', 'SimCLR'])
+        },
+        'il': {
+            'lr': tune.grid_search([0.01, 0.02])
+        }
+    }
+    modified_rep_ex = dict(representation_learning)
+    modified_il_ex = dict(il_train)
 
 
-@represent_ex.main
-def run(pretrain_configs, adapt_configs):
-    for pretrain_config in pretrain_configs:
-        # Run representation pretrain
-        pretrain_run = represent_ex.run(config_updates=pretrain_config)
+@chain_ex.main
+def run(exp_name, run_rep, run_il, spec, modified_rep_ex, modified_il_ex):
+    def runnable_function(inner_exp, modified_inner_ex, exp_name_, spec_):
+        inner_ex_config = sacred_copy(modified_inner_ex)
+        spec_copy = sacred_copy(spec_)
+        print('spec copy', spec_copy)
 
-    for adapt_config in adapt_configs:
-        # Run imitation learning adaptation
-        adapt_run = il_train_ex(config_updates=adapt_config)
+        def run_exp(config, exp_name_):
+            run_experiment(inner_exp, inner_ex_config, config, exp_name_)
+
+        analysis = tune.run(
+            run_exp,
+            name=exp_name_,
+            config=spec_copy
+        )
+        best_config = analysis.get_best_config(metric="accuracy")
+        print(f"Best config is: {best_config}")
+        print("Results available at: ")
+        print(analysis._get_trial_paths())
+
+    if run_rep:
+        runnable_function(represent_ex, modified_rep_ex, f"{exp_name}_rep", spec['rep'])
+    if run_il:
+        runnable_function(il_train_ex, modified_il_ex, f"{exp_name}_il", spec['il'])
 
 
+def main():
+    observer = FileStorageObserver('chain_runs')
+    chain_ex.observers.append(observer)
+    chain_ex.run_commandline()
+
+
+if __name__ == '__main__':
+    main()
