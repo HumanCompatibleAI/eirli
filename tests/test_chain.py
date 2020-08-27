4667d4ce3f5e707e215f5e83f71190e6154fb2e4
Cynthia Chen <RPC2@users.noreply.github.com>
2020-08-27 23:02:30 +0800

    remove policy path

--- tests/test_chain.py
+++ tests/test_chain.py
@@ -1,9 +1,8 @@
-import os
 import copy
 import pytest
-from sacred.observers import FileStorageObserver
 from il_representations.test_support.configuration import CHAIN_CONFIG
 from il_representations.test_support.configuration import BENCHMARK_TEST_CONFIGS
+
 import warnings
 for category in [FutureWarning, DeprecationWarning, PendingDeprecationWarning]:
     warnings.filterwarnings("ignore", category=category)
@@ -20,18 +19,6 @@ def test_chain(benchmark_cfg, chain_ex, file_observer):
     chain_config['spec']['il_train'].update(common_config)
     chain_config['spec']['il_test'].update(common_config)
 
-    final_pol_name = 'last_test_policy.pt'
-    log_dir = file_observer.dir
-    policy_path = os.path.join(log_dir, final_pol_name)
-    chain_config['spec']['policy_path'].update(policy_path)
-
-    observer = FileStorageObserver('runs/chain_runs')
-    chain_ex.observers.append(observer)
     chain_ex.run(config_updates=chain_config)
 
-
-if __name__ == '__main__':
-    from il_representations.scripts.pretrain_n_adapt import chain_ex
-    for bm in BENCHMARK_TEST_CONFIGS:
-        test_chain(bm, chain_ex)
 
