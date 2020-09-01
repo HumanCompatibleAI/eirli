098aa5f498775a139a388a6deb16f3436c27158c
Sam Toyer <sam@qxcv.net>
2020-09-01 16:25:16 -0700

    test_chain should only do some benchmarks

--- tests/test_chain.py
+++ tests/test_chain.py
@@ -1,16 +1,16 @@
-import pytest
 import ray
 
 from il_representations.test_support.configuration import (
     BENCHMARK_TEST_CONFIGS, CHAIN_CONFIG)
 
 
-@pytest.mark.parametrize('benchmark_cfg', BENCHMARK_TEST_CONFIGS)
-def test_chain(benchmark_cfg, chain_ex, file_observer):
+def test_chain(chain_ex, file_observer):
     try:
         chain_ex.run(config_updates={
             **CHAIN_CONFIG,
-            'benchmark': benchmark_cfg,
+            'spec': {
+                'benchmark': ray.tune.grid_search(BENCHMARK_TEST_CONFIGS)
+            }
         })
     finally:
         # always shut down Ray, in case we get a test failure
