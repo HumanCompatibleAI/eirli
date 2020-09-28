f72a7cd4cbc4e50a23ef5d72e589fccf73b7ddc2
Sam Toyer <sam@qxcv.net>
2020-09-27 22:01:54 -0700

    Fix various bugs

--- cloud/submit_phase1.sh
+++ cloud/submit_phase1.sh
@@ -10,7 +10,7 @@ for il in cfg_il_bc_nofreeze; do
         # "control" config without repL
         ray submit --tmux "$cluster_cfg_path" \
             ./submit_pretrain_n_adapt.py \
-            -- $base_cfgs $bench repl_none $il
+            -- $base_cfgs $bench cfg_repl_none $il
         for repl_nc in condition_one_temporal_cpc condition_two_temporal_cpc_momentum \
             condition_three_temporal_cpc_sym_proj condition_four_temporal_cpc_asym_proj \
             condition_five_temporal_cpc_augment_both; do
