10fd23e5b6db0dba6e461eb70ec1ab566383b94e
Cody Wild <codywild@berkeley.edu>
2020-10-09 11:43:24 -0700

    Add an explicit for loop over seeds

--- src/il_representations/scripts/temporal_cpc_comparisons.sh
+++ src/il_representations/scripts/temporal_cpc_comparisons.sh
@@ -2,32 +2,35 @@
 #python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
 #repl.stooke_contrastive_hyperparams_dmc repl.condition_one_temporal_cpc
 
-
+for seed_var in {1..3}:
+do
 # TemporalCPC with augmentations
-python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
-repl.stooke_contrastive_hyperparams_dmc repl.temporal_cpc_augment_both_magical
+  python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
+  repl.stooke_contrastive_hyperparams_dmc repl.temporal_cpc_augment_both_magical seed=$seed_var
 
-# TemporalCPC with Action Conditioning
-python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
-repl.stooke_contrastive_hyperparams_dmc repl.algo=ActionConditionedTemporalCPC\
- repl.use_random_rollouts=False
-
-
- # Regular/Identity CPC
-python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
-repl.stooke_contrastive_hyperparams_dmc repl.identity_cpc
+  # TemporalCPC with Action Conditioning
+  python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
+  repl.stooke_contrastive_hyperparams_dmc repl.algo=ActionConditionedTemporalCPC\
+   repl.use_random_rollouts=False seed=$seed_var
 
 
- # CEB
-python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
-repl.stooke_contrastive_hyperparams_dmc repl.temporal_ceb_no_projection
+   # Regular/Identity CPC
+  python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
+  repl.stooke_contrastive_hyperparams_dmc repl.identity_cpc seed=$seed_var
 
 
-# TemporalVAE
-python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
-repl.stooke_contrastive_hyperparams_dmc repl.condition_thirteen_temporal_vae_lowbeta
+   # CEB
+  python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
+  repl.stooke_contrastive_hyperparams_dmc repl.temporal_ceb_no_projection seed=$seed_var
 
 
-# VAE
-python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
-repl.stooke_contrastive_hyperparams_dmc repl.condition_ten_vae
+  # TemporalVAE
+  python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
+  repl.stooke_contrastive_hyperparams_dmc repl.condition_thirteen_temporal_vae_lowbeta seed=$seed_var
+
+
+  # VAE
+  python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
+  repl.stooke_contrastive_hyperparams_dmc repl.condition_ten_vae seed=$seed_var
+
+done
