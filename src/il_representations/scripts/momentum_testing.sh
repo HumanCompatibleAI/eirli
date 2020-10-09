36a4ea988bce32f0aa8bf39bf3df548ee1f2d673
Cody Wild <codywild@berkeley.edu>
2020-10-09 10:05:14 -0700

    Add xvfb-run command

--- src/il_representations/scripts/momentum_testing.sh
+++ src/il_representations/scripts/momentum_testing.sh
@@ -1,21 +1,21 @@
 
 for seed_var in 1 2 3 :
   #success condition
-  python -m il_representations.scripts.run_rep_learner with algo='MoCo' algo_params.representation_dim=10\
+  CUDA_VISIBLE_DEVICES="1" xvfb-run python -m il_representations.scripts.run_rep_learner with algo='MoCo' algo_params.representation_dim=10\
   benchmark.benchmark_name='dm_control' benchmark.n_envs=1 use_random_rollouts=False \
   pretrain_epochs=50 algo_params.encoder_kwargs.momentum_weight=0.995\
    algo_params.loss_calculator_kwargs.use_batch_neg=True seed=$seed_var
 
 
 
-  python -m il_representations.scripts.run_rep_learner with algo='MoCo' algo_params.representation_dim=10\
+  CUDA_VISIBLE_DEVICES="1" xvfb-run python -m il_representations.scripts.run_rep_learner with algo='MoCo' algo_params.representation_dim=10\
   benchmark.benchmark_name='dm_control' benchmark.n_envs=1 use_random_rollouts=False \
   pretrain_epochs=50 algo_params.encoder_kwargs.momentum_weight=0.995\
    algo_params.loss_calculator_kwargs.use_batch_neg=False seed=$seed_var
 
 
 
-  python -m il_representations.scripts.run_rep_learner with algo='MoCo' algo_params.representation_dim=10\
+  CUDA_VISIBLE_DEVICES="1" xvfb-run python -m il_representations.scripts.run_rep_learner with algo='MoCo' algo_params.representation_dim=10\
   benchmark.benchmark_name='dm_control' benchmark.n_envs=1 use_random_rollouts=False \
   pretrain_epochs=50 algo_params.encoder_kwargs.momentum_weight=0.50\
    algo_params.loss_calculator_kwargs.use_batch_neg=False seed=$seed_var
