32022cda658a93bba033771c92bcddfb377e9cca
Cody Wild <codywild@berkeley.edu>
2020-10-09 15:50:09 -0700

    test with mw=95, bn=True

--- src/il_representations/scripts/momentum_testing.sh
+++ src/il_representations/scripts/momentum_testing.sh
@@ -20,9 +20,14 @@ do
 #  pretrain_epochs=50 algo_params.encoder_kwargs.momentum_weight=0.50\
 #   algo_params.loss_calculator_kwargs.use_batch_neg=False seed=$seed_var
 #
-  CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.run_rep_learner with algo='MoCo' algo_params.representation_dim=10\
+#  CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.run_rep_learner with algo='MoCo' algo_params.representation_dim=10\
+#  benchmark.benchmark_name='dm_control' benchmark.n_envs=1 use_random_rollouts=False \
+#  pretrain_epochs=50 algo_params.encoder_kwargs.momentum_weight=0.95\
+#   algo_params.loss_calculator_kwargs.use_batch_neg=False seed=$seed_var
+
+    CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.run_rep_learner with algo='MoCo' algo_params.representation_dim=10\
   benchmark.benchmark_name='dm_control' benchmark.n_envs=1 use_random_rollouts=False \
   pretrain_epochs=50 algo_params.encoder_kwargs.momentum_weight=0.95\
-   algo_params.loss_calculator_kwargs.use_batch_neg=False seed=$seed_var
+   algo_params.loss_calculator_kwargs.use_batch_neg=True seed=$seed_var
 
 done
