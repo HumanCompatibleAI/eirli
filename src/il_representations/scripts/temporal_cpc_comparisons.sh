88cc10c7e4486462d45a0eb19c3fa7e8f773bae5
Cody Wild <codywild@berkeley.edu>
2020-10-09 14:30:58 -0700

    Hotfix to MatchRegions data location

--- src/il_representations/scripts/temporal_cpc_comparisons.sh
+++ src/il_representations/scripts/temporal_cpc_comparisons.sh
@@ -2,35 +2,35 @@
 #python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
 #repl.stooke_contrastive_hyperparams_dmc repl.condition_one_temporal_cpc
 
-for seed_var in {1..3}:
+for seed_var in {1..3}
 do
 # TemporalCPC with augmentations
-  python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
+  CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
   repl.stooke_contrastive_hyperparams_dmc repl.temporal_cpc_augment_both_magical seed=$seed_var
 
   # TemporalCPC with Action Conditioning
-  python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
+  CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
   repl.stooke_contrastive_hyperparams_dmc repl.algo=ActionConditionedTemporalCPC\
    repl.use_random_rollouts=False seed=$seed_var
 
 
    # Regular/Identity CPC
-  python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
+  CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
   repl.stooke_contrastive_hyperparams_dmc repl.identity_cpc seed=$seed_var
 
 
    # CEB
-  python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
+  CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
   repl.stooke_contrastive_hyperparams_dmc repl.temporal_ceb_no_projection seed=$seed_var
 
 
   # TemporalVAE
-  python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
+  CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
   repl.condition_thirteen_temporal_vae_lowbeta seed=$seed_var
 
 
   # VAE
-  python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
+  CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt with cfg_use_magical\
   repl.condition_ten_vae seed=$seed_var
 
 done
