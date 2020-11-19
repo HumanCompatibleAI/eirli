5b3487505b3eedf29518ec238a75520fc6d3fe04
Sam Toyer <sam@qxcv.net>
2020-11-18 17:34:09 -0800

    implement mkdataset_random

--- src/il_representations/scripts/convert_all_data_to_new_format.sh
+++ src/il_representations/scripts/convert_all_data_to_new_format.sh
@@ -5,20 +5,38 @@
 set -e
 
 gen_demos() {
-    xvfb-run -a python -m il_representations.scripts.mkdataset_demos $@
+    xvfb-run -a python -m il_representations.scripts.mkdataset_demos with n_traj_total=1 $@
+}
+
+gen_random() {
+    xvfb-run -a python -m il_representations.scripts.mkdataset_random with n_timesteps_min=64 $@
 }
 
 for mag_pfx in ClusterColour ClusterShape FixColour FindDupe MatchRegions MakeLine MoveToCorner MoveToRegion; do
-    echo "Converting MAGICAL/${mag_pfx}"
-    gen_demos with env_cfg.benchmark_name=magical env_cfg.magical_env_prefix="${mag_pfx}" n_traj_total=2
+    echo "Working on MAGICAL/${mag_pfx}"
+    mag_env_opts=(env_cfg.benchmark_name=magical env_cfg.magical_env_prefix="${mag_pfx}")
+    echo "Demonstrations:"
+    gen_demos ${mag_env_opts[@]}
+    echo "Random rollouts:"
+    gen_random ${mag_env_opts[@]}
 done
 
 for dmc_name in finger-spin cheetah-run walker-walk cartpole-swingup reacher-easy ball-in-cup-catch; do
-    echo "Converting DMC/${dmc_name}"
-    gen_demos with env_cfg.benchmark_name=dm_control env_cfg.dm_control_env_name="${dmc_name}" n_traj_total=2
+    echo "Working on DMC/${dmc_name}"
+    dmc_env_opts=(env_cfg.benchmark_name=dm_control env_cfg.dm_control_env_name="${dmc_name}")
+    echo "Demonstrations:"
+    gen_demos ${dmc_env_opts[@]}
+    echo "Random rollouts:"
+    gen_random ${dmc_env_opts[@]}
 done
 
 for atari_name in PongNoFrameskip-v4; do
-    echo "Converting Atari/${atari_name}"
-    gen_demos with env_cfg.benchmark_name=atari env_cfg.atari_env_id="${atari_name}" n_traj_total=2
+    echo "Working on Atari/${atari_name}"
+    atari_env_opts=(env_cfg.benchmark_name=atari env_cfg.atari_env_id="${atari_name}")
+    echo "Demonstrations:"
+    gen_demos ${atari_env_opts[@]}
+    echo "Random rollouts:"
+    gen_random ${atari_env_opts[@]}
 done
+
+echo "Done!"
