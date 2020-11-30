d9611198a6a552bd5b8e8ed1215665a027fc5cd5
Cody Wild <codywild@berkeley.edu>
2020-11-30 15:38:20 -0800

    Remove unused comments

--- src/il_representations/scripts/pretrain_n_adapt.py
+++ src/il_representations/scripts/pretrain_n_adapt.py
@@ -733,9 +733,6 @@ def run(exp_name, metric, spec, repl, il_train, il_test, env_cfg, env_data,
     env_data_config['data_root'] = os.path.abspath(
         os.path.join(cwd, env_data_config['data_root']))
 
-    # these are config keys shared between all two or more of the three Sacred
-    # experiments (repl, il_train, il_test)
-
     def trainable_function(config):
         # "config" argument is passed in by Ray Tune
         shared_configs = {'env_cfg', 'env_data', 'venv_opts'}
