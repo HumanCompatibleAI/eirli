1ad104bdf561967bd5cc52a2db5e8689c5eea2ef
Cody Wild <codywild@berkeley.edu>
2020-12-17 15:12:26 -0800

    Explicitly install old Java JDK

--- minecraft_setup.sh
+++ minecraft_setup.sh
@@ -1,6 +1,13 @@
 # A script to run within Docker to setup Minecraft
 add-apt-repository ppa:openjdk-r/ppa
 apt-get update
+
+apt-get purge openjdk-8*
+
+apt-get install openjdk-8-jre-headless=8u162-b12-1
+apt-get install openjdk-8-jdk-headless=8u162-b12-1
+apt-get install openjdk-8-jre=8u162-b12-1
+apt-get install openjdk-8-jdk=8u162-b12-1
 apt-get install -y openjdk-8-jdk
 
 git clone --recurse-submodules https://github.com/decodyng/minerl.git
