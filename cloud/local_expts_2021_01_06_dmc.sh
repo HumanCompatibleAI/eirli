d5cf3505fb6dc70fa0f3bbf4bd9f94e2a1675bb8
Sam Toyer <sam@qxcv.net>
2021-01-06 21:36:54 -0800

    Fix local DMC script

--- cloud/local_expts_2021_01_06_dmc.sh
+++ cloud/local_expts_2021_01_06_dmc.sh
@@ -6,10 +6,10 @@ set -e
 
 # override three-seed config to do five seeds instead. Also make sure that
 # HalfCheetah always uses the extended dataset.
-base_cfgs="cfg_base_3seed_1cpu_pt5gpu_2envs tune_run_kwargs.num_samples=5 cfg_data_il_hc_extended"
+base_cfgs="cfg_base_3seed_1cpu_pt5gpu_2envs tune_run_kwargs.num_samples=5 ray_init_kwargs.num_cpus=8 cfg_data_il_hc_extended"
 cluster_cfg_path="./gcp_cluster_sam.yaml"
 
-for il in cfg_il_bc_nofreeze cfg_il_bc_500k_nofreeze; do
+for il in cfg_il_bc_500k_nofreeze; do
     froco="$([[ "$il" == *_nofreeze  ]] || echo '_froco')"
 
     # we have no multitask runs because DMC does not support multitask
@@ -17,8 +17,8 @@ for il in cfg_il_bc_nofreeze cfg_il_bc_500k_nofreeze; do
         "env_cfg.benchmark_name=dm_control env_cfg.task_name=cheetah-run" \
         "env_cfg.benchmark_name=dm_control env_cfg.task_name=finger-spin" \
         ; do
-        python -m il_representations.scripts.pretrain_n_adapt \
-            -- $base_cfgs $bench cfg_repl_none cfg_data_il_hc_extended $il exp_ident=control_il$froco
+        python -m il_representations.scripts.pretrain_n_adapt with \
+            $base_cfgs $bench cfg_repl_none cfg_data_il_hc_extended $il exp_ident=control_il$froco
 
         for repl in \
             "exp_ident=inv_dyn_rand_demos_il$froco cfg_data_repl_demos_random cfg_repl_inv_dyn" \
@@ -36,8 +36,8 @@ for il in cfg_il_bc_nofreeze cfg_il_bc_500k_nofreeze; do
             "exp_ident=ac_cpc_aug_rand_demos_il$froco cfg_data_repl_demos_random repl.stooke_contrastive_hyperparams_dmc repl.identity_cpc_action_conditioned_augment_both repl.algo_params.batch_size=192" \
             "exp_ident=ac_cpc_aug_rand_only_il$froco cfg_data_repl_random repl.stooke_contrastive_hyperparams_dmc repl.identity_cpc_action_conditioned_augment_both repl.algo_params.batch_size=192" \
             ; do
-            python -m il_representations.scripts.pretrain_n_adapt \
-                -- $base_cfgs $bench cfg_base_repl_5000_batches cfg_force_use_repl $il $repl
+            python -m il_representations.scripts.pretrain_n_adapt with \
+                $base_cfgs $bench cfg_base_repl_5000_batches cfg_force_use_repl $il $repl
         done
     done
 done
