8bb8a9af76d0e120aa39c6435e2a3bd72a3420f2
Cody Wild <codywild@berkeley.edu>
2021-01-07 13:57:39 -0800

    Clean up for final tests

--- src/il_representations/algos/representation_learner.py
+++ src/il_representations/algos/representation_learner.py
@@ -300,8 +300,7 @@ class RepresentationLearner(BaseEnvironmentLearner):
 
         dataloader = DataLoader(interleaved_dataset,
                                 num_workers=self.dataset_max_workers,
-                                batch_size=int(self.batch_size),
-                                shuffle=False) # TODO testing, change this back
+                                batch_size=int(self.batch_size)) # TODO testing, change this back
 
         loss_record = []
 
