3aa718ab470f1bf946ad502637acfb4b0dd75b96
Cody Wild <codywild@berkeley.edu>
2021-01-14 11:19:13 -0800

    Continue consistency checking pt2

--- tests/test_chain.py
+++ tests/test_chain.py
@@ -104,7 +104,7 @@ def test_hash_config():
     chain_config = copy.deepcopy(CHAIN_CONFIG)
     repl_config = chain_config['repl']
     repl_config['algo'] = algos.TemporalCPC
-    #import pdb; pdb.set_trace()
+    import pdb; pdb.set_trace()
     config_hash_1 = hash_configs(repl_config)
     config_hash_2 = hash_configs(repl_config)
     repl_config['algo'] = algos.SimCLR
