c0752a8a0880d21bf44afe481153b96e789982cb
Cody Wild <codywild@berkeley.edu>
2021-01-19 16:42:59 -0800

    Use updated task name parameter

--- src/il_representations/scripts/icml_tuning.sh
+++ src/il_representations/scripts/icml_tuning.sh
@@ -9,11 +9,11 @@ for algo_config in ${tuning_configs[@]}; do
   for dmc_env in ${dmc_envs[@]}; do
     printf "\n ***TRAINING $algo_config ON  $dmc_env *** \n "
     CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt\
-     with icml_tuning cfg_use_dm_control $algo_config exp_ident=$algo_config benchmark.dm_control_env=$dmc_env
+     with icml_tuning cfg_use_dm_control $algo_config exp_ident=$algo_config env_cfg.task_name=$dmc_env
   done
   for magical_env in ${magical_envs[@]}; do
     printf "\n ***TRAINING $algo_config ON $magical_env *** \n "
     CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt\
-    with icml_tuning cfg_use_magical $algo_config exp_ident=$algo_config benchmark.magical_env_prefix=$magical_env
+    with icml_tuning cfg_use_magical $algo_config exp_ident=$algo_config env_cfg.task_name=$magical_env
   done
 done
