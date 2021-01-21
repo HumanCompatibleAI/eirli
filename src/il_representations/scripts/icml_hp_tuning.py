5b9144219d5f547563d9e524f9807cdfbcc23654
Cody Wild <codywild@berkeley.edu>
2021-01-21 15:45:29 -0800

    Change default VAE beta value

--- src/il_representations/scripts/icml_hp_tuning.py
+++ src/il_representations/scripts/icml_hp_tuning.py
@@ -119,7 +119,7 @@ def make_icml_tuning_configs(experiment_obj):
             ('il_test:n_rollouts', [20])
         ])
         skopt_ref_configs = [
-            {'repl:algo_params:loss_calculator_kwargs:beta': 0.01,
+            {'repl:algo_params:loss_calculator_kwargs:beta': 1e-5,
              'il_test:n_rollouts': 20
              }]
 
