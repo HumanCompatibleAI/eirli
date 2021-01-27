2f9b2a8107a9003dc42bd269fe7ccc4b9e292d07
Sam Toyer <sam@qxcv.net>
2021-01-27 13:40:59 -0800

    Update local expt script

--- cloud/local_expts_2021_01_26_col_aug.sh
+++ cloud/local_expts_2021_01_26_col_aug.sh
@@ -6,7 +6,8 @@
 set -e
 
 # probably we need 5 seeds to get something meaningful
-base_cfgs="cfg_base_3seed_1cpu_pt5gpu_2envs tune_run_kwargs.num_samples=5 ray_init_kwargs.num_cpus=1"
+# (note that this requires Ray to be running on port 42000)
+base_cfgs="cfg_base_5seed_1cpu_pt3gpu ray_init_kwargs.address=localhost:42000 venv_opts.n_envs=5 "
 
 for il in cfg_il_bc_20k_nofreeze; do
     froco="$([[ "$il" == *_nofreeze  ]] || echo '_froco')"
@@ -22,6 +23,9 @@ for il in cfg_il_bc_20k_nofreeze; do
             "exp_ident=cpc_identity_aug_rand_demos_il$froco cfg_data_repl_demos_random repl.stooke_contrastive_hyperparams_dmc repl.identity_cpc_aug repl.algo_params.batch_size=192" \
             "exp_ident=cpc_identity_aug_rand_demos_col_aug_il$froco cfg_data_repl_demos_random repl.stooke_contrastive_hyperparams_dmc repl.identity_cpc_aug repl.algo_params.batch_size=192 repl.algo_params.augmenter_kwargs.augmenter_spec='translate,rotate,gaussian_blur,color_jitter_ex'" \
             "exp_ident=cpc_identity_aug_rand_demos_il_col_aug$froco cfg_data_repl_demos_random repl.stooke_contrastive_hyperparams_dmc repl.identity_cpc_aug repl.algo_params.batch_size=192 il_train.bc.augs='rotate,translate,noise,color_jitter_ex'" \
+            "exp_ident=tcpc_aug_mt_il_25traj$froco cfg_data_repl_demos_magical_mt repl.stooke_contrastive_hyperparams_dmc repl.temporal_cpc_augment_both_magical" \
+            "exp_ident=tcpc_aug_mt_col_aug_il_25traj$froco cfg_data_repl_demos_magical_mt repl.stooke_contrastive_hyperparams_dmc repl.temporal_cpc_augment_both_magical repl.algo_params.augmenter_kwargs.augmenter_spec='translate,rotate,gaussian_blur,color_jitter_ex'" \
+            "exp_ident=tcpc_aug_mt_il_col_aug_25traj$froco cfg_data_repl_demos_magical_mt repl.stooke_contrastive_hyperparams_dmc repl.temporal_cpc_augment_both_magical il_train.bc.augs='rotate,translate,noise,color_jitter_ex'" \
             ; do
             python -m il_representations.scripts.pretrain_n_adapt with \
                 $base_cfgs $bench cfg_base_repl_5000_batches cfg_force_use_repl $il $repl &
@@ -34,4 +38,4 @@ for il in cfg_il_bc_20k_nofreeze; do
     done
 done
 
-disown
+wait
--- src/il_representations/scripts/chain_configs.py
+++ src/il_representations/scripts/chain_configs.py
@@ -52,6 +52,21 @@ def make_chain_configs(experiment_obj):
         _ = locals()
         del _
 
+    @experiment_obj.named_config
+    def cfg_base_5seed_1cpu_pt3gpu():
+        use_skopt = False
+        tune_run_kwargs = dict(num_samples=5,
+                               # retry on (node) failure
+                               max_failures=2,
+                               fail_fast=False,
+                               resources_per_trial=dict(
+                                   cpu=1,
+                                   gpu=0.32,
+                               ))
+
+        _ = locals()
+        del _
+
 
     @experiment_obj.named_config
     def cfg_base_skopt_4cpu_pt3gpu_no_retry():
