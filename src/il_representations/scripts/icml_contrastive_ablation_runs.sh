4d829f02f71e84520372aeec2f4610ed640a7316
Cody Wild <codywild@berkeley.edu>
2021-02-02 18:31:56 -0800

    Add ablation to exp ident

--- src/il_representations/scripts/icml_contrastive_ablation_runs.sh
+++ src/il_representations/scripts/icml_contrastive_ablation_runs.sh
@@ -16,13 +16,13 @@ for dmc_env in ${dmc_envs[@]}; do
          CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt\
          with icml_il_on_repl_sweep cfg_use_dm_control\
          $test_config $dataset_config cfg_il_bc_200k_nofreeze\
-         exp_ident=$test_config"_"$dataset_config env_cfg.task_name=$dmc_env
+         exp_ident="ablation_"$test_config"_"$dataset_config env_cfg.task_name=$dmc_env
         done
         # control run
         CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt\
          with cfg_use_dm_control icml_control\
           $dataset_config cfg_il_bc_200k_nofreeze\
-          exp_ident="control_"$dataset_config env_cfg.task_name=$dmc_env
+          exp_ident="ablation_control_"$dataset_config env_cfg.task_name=$dmc_env
     done
 done
 
@@ -35,13 +35,13 @@ for magical_env in ${magical_envs[@]}; do
               CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt\
               with icml_il_on_repl_sweep cfg_use_magical\
               $test_config $dataset_config cfg_il_bc_20k_nofreeze  \
-              exp_ident=$test_config"_"$dataset_config env_cfg.task_name=$magical_env
+              exp_ident="ablation_"$test_config"_"$dataset_config env_cfg.task_name=$magical_env
         done
 
         # control run
         CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt\
               with cfg_use_magical icml_control\
               $dataset_config cfg_il_bc_20k_nofreeze  \
-              exp_ident="control_"$dataset_config env_cfg.task_name=$magical_env
+              exp_ident="ablation_control_"$dataset_config env_cfg.task_name=$magical_env
     done
 done
