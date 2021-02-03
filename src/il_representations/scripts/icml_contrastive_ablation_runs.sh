9b13c3dfb744d0e3abf32c3e1cd0946e5caf244f
Sam Toyer <qxcv@users.noreply.github.com>
2021-02-02 21:12:02 -0800

    Spawn MAGICAL first, do one control per env

--- src/il_representations/scripts/icml_contrastive_ablation_runs.sh
+++ src/il_representations/scripts/icml_contrastive_ablation_runs.sh
@@ -8,40 +8,36 @@ declare -a magical_envs=("MatchRegions" "MoveToCorner" "MoveToRegion")
 
 declare -a dataset_configs=("cfg_data_repl_random")
 
+for magical_env in ${magical_envs[@]}; do
+    for dataset_config in ${dataset_configs[@]}; do
+        for test_config in ${testing_configs[@]}; do
+            echo -e "\n ***TRAINING $algo_config ON $magical_env *** \n "
+              CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt\
+              with icml_il_on_repl_sweep cfg_use_magical\
+              "$test_config" "$dataset_config" cfg_il_bc_20k_nofreeze  \
+              exp_ident="ablation_${test_config}_${dataset_config}" "env_cfg.task_name=$magical_env"
+        done
+    done
+    # control run
+    CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt\
+          with cfg_use_magical icml_control\
+          "$dataset_config" cfg_il_bc_20k_nofreeze  \
+          exp_ident="ablation_control" "env_cfg.task_name=$magical_env"
+done
 
 for dmc_env in ${dmc_envs[@]}; do
     for dataset_config in ${dataset_configs[@]}; do
         for test_config in ${testing_configs[@]}; do
-        printf "\n ***TRAINING $algo_config ON  $dmc_env *** \n "
+        echo -e "\n ***TRAINING $algo_config ON  $dmc_env *** \n "
          CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt\
          with icml_il_on_repl_sweep cfg_use_dm_control\
-         $test_config $dataset_config cfg_il_bc_200k_nofreeze\
-         exp_ident="ablation_"$test_config"_"$dataset_config env_cfg.task_name=$dmc_env
+         "$test_config" "$dataset_config" cfg_il_bc_200k_nofreeze\
+         exp_ident="ablation_${test_config}_${dataset_config}" "env_cfg.task_name=$dmc_env"
         done
-        # control run
-        CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt\
-         with cfg_use_dm_control icml_control\
-          $dataset_config cfg_il_bc_200k_nofreeze\
-          exp_ident="ablation_control_"$dataset_config env_cfg.task_name=$dmc_env
     done
-done
-
-
-
-for magical_env in ${magical_envs[@]}; do
-    for dataset_config in ${dataset_configs[@]}; do
-        for test_config in ${testing_configs[@]}; do
-            printf "\n ***TRAINING $algo_config ON $magical_env *** \n "
-              CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt\
-              with icml_il_on_repl_sweep cfg_use_magical\
-              $test_config $dataset_config cfg_il_bc_20k_nofreeze  \
-              exp_ident="ablation_"$test_config"_"$dataset_config env_cfg.task_name=$magical_env
-        done
-
-        # control run
-        CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt\
-              with cfg_use_magical icml_control\
-              $dataset_config cfg_il_bc_20k_nofreeze  \
-              exp_ident="ablation_control_"$dataset_config env_cfg.task_name=$magical_env
-    done
+    # control run
+    CUDA_VISIBLE_DEVICES="1" xvfb-run -a python -m il_representations.scripts.pretrain_n_adapt\
+     with cfg_use_dm_control icml_control\
+      "$dataset_config" cfg_il_bc_200k_nofreeze\
+      exp_ident="ablation_control" "env_cfg.task_name=$dmc_env"
 done
