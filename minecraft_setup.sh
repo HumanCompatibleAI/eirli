b469aa0aa391351f8500d24d3b6ee22955f7c9f5
Cody Wild <codywild@berkeley.edu>
2021-02-22 13:14:27 -0800

    Temporarily switch to fix_process_action branch

--- minecraft_setup.sh
+++ minecraft_setup.sh
@@ -11,4 +11,6 @@ apt-get install -y openjdk-8-jdk=8u162-b12-1
 
 git clone --recurse-submodules https://github.com/HumanCompatibleAI/minerl.git
 cd minerl
+# TODO change this when fix_process_action is merged, which should happen before this PR gets merged
+git checkout fix_proccess_action
 CFLAGS="-I/opt/conda/include" pip install --no-cache-dir -e .
