e8e03c157ba4b0c729fd9f3a39258909c9dbfbf7
Cody Wild <codywild@berkeley.edu>
2021-03-15 16:00:25 -0700

    Turn print into info()

--- src/il_representations/scripts/il_train.py
+++ src/il_representations/scripts/il_train.py
@@ -36,7 +36,7 @@ from il_representations.scripts.utils import print_policy_info
 try:
     import realistic_benchmarks.policies as rb_policies
 except ImportError:
-    print("Realistic Benchmarks is not installed; as a result much Minecraft functionality will not work")
+    logging.info("Realistic Benchmarks is not installed; as a result much Minecraft functionality will not work")
 
 
 bc_ingredient = Ingredient('bc')
