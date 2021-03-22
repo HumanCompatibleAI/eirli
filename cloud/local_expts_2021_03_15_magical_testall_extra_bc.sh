b76980dee7f56ea2dbce2504750d209dc52dcc28
Sam Toyer <sam@qxcv.net>
2021-03-22 15:35:45 -0700

    Appease is_multitask

--- cloud/local_expts_2021_03_15_magical_testall_extra_bc.sh
+++ cloud/local_expts_2021_03_15_magical_testall_extra_bc.sh
@@ -6,10 +6,13 @@
 set -e
 
 base_cfgs=("cfg_base_5seed_1cpu_pt25gpu" "tune_run_kwargs.num_samples=5" "ray_init_kwargs.address=localhost:42000"
-           "tune_run_kwargs.resources_per_trial.gpu=0.5")
+           "tune_run_kwargs.resources_per_trial.gpu=0.25")
 
 # let's cast a wiiiide net
-declare -a repl_configs=("icml_inv_dyn" "icml_dynamics" "icml_identity_cpc" "icml_vae")
+# (update: disabling identity CPC because it uses lots of memory, and svm is
+# currently running low)
+# declare -a repl_configs=("icml_inv_dyn" "icml_dynamics" "icml_identity_cpc" "icml_vae")
+declare -a repl_configs=("icml_inv_dyn" "icml_dynamics" "icml_vae")
 
 submit_expt() {
     # submit experiments to local Ray server
@@ -31,26 +34,26 @@ launch_for_env exp_ident="mtest_st_demos_matchregions" env_cfg.task_name=MatchRe
 launch_for_env exp_ident="mtest_st_rand_demos_matchregions" env_cfg.task_name=MatchRegions-Demo-v0 \
     cfg_data_repl_demos_random
 launch_for_env exp_ident="mtest_st_test_demos_matchregions" env_cfg.task_name=MatchRegions-Demo-v0 \
-    cfg_data_repl_matchregions_demos_magical_test
+    cfg_data_repl_matchregions_demos_magical_test repl.is_multitask=True
 launch_for_env exp_ident="mtest_st_test_rand_demos_matchregions" env_cfg.task_name=MatchRegions-Demo-v0 \
-    cfg_data_repl_matchregions_rand_demos_magical_test
+    cfg_data_repl_matchregions_rand_demos_magical_test repl.is_multitask=True
 
 launch_for_env exp_ident="mtest_st_demos_movetoregion" env_cfg.task_name=MoveToRegion-Demo-v0 \
                cfg_data_repl_demos
 launch_for_env exp_ident="mtest_st_rand_demos_movetoregion" env_cfg.task_name=MoveToRegion-Demo-v0 \
                cfg_data_repl_demos_random
 launch_for_env exp_ident="mtest_st_test_demos_movetoregion" env_cfg.task_name=MoveToRegion-Demo-v0 \
-               cfg_data_repl_movetoregion_demos_magical_test
+               cfg_data_repl_movetoregion_demos_magical_test repl.is_multitask=True
 launch_for_env exp_ident="mtest_st_test_rand_demos_movetoregion" env_cfg.task_name=MoveToRegion-Demo-v0 \
-               cfg_data_repl_movetoregion_rand_demos_magical_test
+               cfg_data_repl_movetoregion_rand_demos_magical_test repl.is_multitask=True
 
 launch_for_env exp_ident="mtest_st_demos_movetocorner" env_cfg.task_name=MoveToCorner-Demo-v0 \
                cfg_data_repl_demos
 launch_for_env exp_ident="mtest_st_rand_demos_movetocorner" env_cfg.task_name=MoveToCorner-Demo-v0 \
                cfg_data_repl_demos_random
 launch_for_env exp_ident="mtest_st_test_demos_movetocorner" env_cfg.task_name=MoveToCorner-Demo-v0 \
-               cfg_data_repl_movetocorner_demos_magical_test
+               cfg_data_repl_movetocorner_demos_magical_test repl.is_multitask=True
 launch_for_env exp_ident="mtest_st_test_rand_demos_movetocorner" env_cfg.task_name=MoveToCorner-Demo-v0 \
-               cfg_data_repl_movetocorner_rand_demos_magical_test
+               cfg_data_repl_movetocorner_rand_demos_magical_test repl.is_multitask=True
 
 wait
