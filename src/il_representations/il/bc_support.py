d7079703235863bea9b562036c90862628271c8c
Cynthia Chen <RPC2@users.noreply.github.com>
2021-04-01 11:17:09 +0800

    update policy saving

--- src/il_representations/il/bc_support.py
+++ src/il_representations/il/bc_support.py
@@ -6,17 +6,20 @@ import torch as th
 
 class BCModelSaver:
     """Callback that saves BC policy every N epochs."""
-    def __init__(self, policy, save_dir, save_interval_batches):
+    def __init__(self, policy, save_dir, save_interval_epochs):
         self.policy = policy
         self.save_dir = save_dir
-        self.last_save_batches = 0
-        self.save_interval_batches = save_interval_batches
+        self.last_save_epochs = 0
+        self.save_interval_epochs = save_interval_epochs
+        self.epoch_count = 0
 
-    def __call__(self, *, batch_num, **kwargs):
+    def __call__(self, **kwargs):
         """It is assumed that this is called on epoch end."""
-        if batch_num >= self.last_save_batches + self.save_interval_batches:
+        self.epoch_count += 1
+        if self.epoch_count >= self.last_save_epochs + self.save_interval_epochs:
             os.makedirs(self.save_dir, exist_ok=True)
-            save_fn = f'policy_{batch_num:07d}_batches.pt'
+            save_fn = f'policy_{self.epoch_count:04d}_epochs.pt'
             save_path = os.path.join(self.save_dir, save_fn)
             th.save(self.policy, save_path)
-            self.last_save_batches = batch_num
+            print(f"Saved policy to {save_path}!")
+            self.last_save_epochs = self.epoch_count
