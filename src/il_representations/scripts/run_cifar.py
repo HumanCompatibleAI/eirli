d1463f5a877a02b79e7a7c23128202a68fd83399
Cody Wild <codywild@berkeley.edu>
2021-04-22 10:43:25 -0700

    No longer expect a tuple in KNN code

--- src/il_representations/scripts/run_cifar.py
+++ src/il_representations/scripts/run_cifar.py
@@ -261,7 +261,7 @@ def test(net, memory_data_loader, test_data_loader, k, num_classes, temperature,
     with torch.no_grad():
         # generate feature bank
         for data, _, target in tqdm(memory_data_loader, desc='Feature extracting'):
-            feature, out = net(data.cuda(non_blocking=True))
+            feature = net(data.cuda(non_blocking=True))
             feature_bank.append(feature)
         # [D, N]
         feature_bank = torch.cat(feature_bank, dim=0).t().contiguous()
@@ -271,7 +271,7 @@ def test(net, memory_data_loader, test_data_loader, k, num_classes, temperature,
         test_bar = tqdm(test_data_loader)
         for data, _, target in test_bar:
             data, target = data.cuda(non_blocking=True), target.cuda(non_blocking=True)
-            feature, out = net(data)
+            feature = net(data)
 
             total_num += data.size(0)
             # compute cos similarity between each feature vector and feature bank ---> [B, N]
