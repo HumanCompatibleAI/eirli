01473d80ebb7529e317a46a939a8694b24f1fbd6
Cynthia Chen <RPC2@users.noreply.github.com>
2021-05-06 13:01:20 +0800

    Try to use 3e-4 lr for SimCLR repl

--- src/il_representations/scripts/chain_configs.py
+++ src/il_representations/scripts/chain_configs.py
@@ -302,6 +302,7 @@ def make_chain_configs(experiment_obj):
         stages_to_run = StagesToRun.REPL_AND_IL
         repl = {
             'algo': 'SimCLR',
+            'optimizer_kwargs': {'lr': 3e-4},
         }
 
         _ = locals()
