7e67b54821c256b434d4b53a4ae250acc8a2217e
Cynthia Chen <RPC2@users.noreply.github.com>
2021-05-12 09:36:01 +0800

    Adjust encoder network channel

--- src/il_representations/algos/encoders.py
+++ src/il_representations/algos/encoders.py
@@ -271,21 +271,20 @@ class MAGICALCNN(nn.Module):
 
 
 class SimCLRModel(nn.Module):
-    def __init__(self, feature_dim=128):
+    def __init__(self, observation_space, representation_dim=128):
         super(SimCLRModel, self).__init__()
 
         self.f = []
+        in_channel = observation_space.shape[0]
         for name, module in resnet50().named_children():
             if name == 'conv1':
-                module = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+                module = nn.Conv2d(in_channel, 64, kernel_size=3, stride=1, padding=1, bias=False)
             if not isinstance(module, nn.Linear) and not isinstance(module, nn.MaxPool2d):
                 self.f.append(module)
         # encoder
         # Temporarily add an extra layer to be closer to our model implementation
         self.f = nn.Sequential(*self.f)
-        # # projection head
-        # self.g = nn.Sequential(nn.Linear(2048, 512, bias=False), nn.BatchNorm1d(512),
-        #                        nn.ReLU(inplace=True), nn.Linear(512, feature_dim, bias=True))
+
 
     def forward(self, x):
         x = self.f(x)
