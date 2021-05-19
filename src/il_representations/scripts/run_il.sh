6a0e105ee2b7adfe3e95d29407c23788dbab49e4
Cynthia <cyn0531@hku.hk>
2021-05-18 20:58:42 -0700

    Script for running MAGICAL without augs

--- src/il_representations/scripts/run_il.sh
+++ src/il_representations/scripts/run_il.sh
@@ -4,12 +4,13 @@ CUDA_VISIBLE_DEVICES=3 xvfb-run -a python src/il_representations/scripts/pretrai
  cfg_base_3seed_1cpu_pt2gpu_2envs \
  cfg_repl_none \
  cfg_il_bc_nofreeze \
+ cfg_bench_short_sweep_magical \
  tune_run_kwargs.num_samples=1 \
- tune_run_kwargs.resources_per_trial.gpu=0.5 \
- exp_ident=coinrun-no-augs \
- il_train.bc.n_batches=400000 \
- il_train.bc.batch_size=512 \
- il_train.bc.augs=None \
- env_cfg.benchmark_name=procgen \
- env_cfg.task_name=coinrun
+ tune_run_kwargs.resources_per_trial.gpu=0.25 \
+ exp_ident=magical-no-augs \
+ il_train.bc.n_batches=100000 \
+ il_train.bc.augs=None
+ # env_cfg.benchmark_name=procgen \
+ # env_cfg.task_name=coinrun
+ # il_train.bc.batch_size=512 \
 
