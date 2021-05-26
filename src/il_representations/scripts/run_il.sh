6c339783d64e784ba6b87a972a470f4126d33f49
Cynthia <cyn0531@hku.hk>
2021-05-25 19:55:33 -0700

    Temporary adjustment to the running script

--- src/il_representations/scripts/run_il.sh
+++ src/il_representations/scripts/run_il.sh
@@ -3,14 +3,11 @@
 CUDA_VISIBLE_DEVICES=1 xvfb-run -a python src/il_representations/scripts/pretrain_n_adapt.py with \
  cfg_repl_none \
  cfg_il_bc_nofreeze \
- cfg_bench_micro_sweep_dm_control \
- cfg_run_few_trajs_long_dm_control \
  il_train.bc.n_trajs=10 \
- exp_ident=dmc_long_ntrajs_10 \
+ exp_ident=test-il-test \
  tune_run_kwargs.num_samples=1 \
  tune_run_kwargs.resources_per_trial.gpu=0.3 \
- # il_train.bc.n_batches=400000 \
- # il_train.bc.nominal_length=10000 \
- # env_cfg.benchmark_name=dm_control \
- # env_cfg.task_name=finger-spin
-
+ il_train.bc.n_batches=1 \
+ env_cfg.benchmark_name=dm_control \
+ env_cfg.task_name=finger-spin
+ # il_train.bc.n_trajs=10 \
