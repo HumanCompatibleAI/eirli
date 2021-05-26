b773ceb7dcf401220ab3f1518e342fda10c6b794
Chen Xin <cyn0531@slurm-q01-inst-1.gpufarm.cs.hku.hk>
2021-05-26 18:17:34 +0800

    Script for DMC runs without augs

--- src/il_representations/scripts/run_il.sh
+++ src/il_representations/scripts/run_il.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=1 xvfb-run -a python src/il_representations/scripts/pretrain_n_adapt.py with \
+CUDA_VISIBLE_DEVICES=0 python src/il_representations/scripts/pretrain_n_adapt.py with \
  cfg_repl_none \
  cfg_il_bc_nofreeze \
  cfg_run_few_trajs_2m_updates \
  cfg_bench_micro_sweep_dm_control \
- exp_ident=dmc-few-trajs-augs \
+ exp_ident=dmc-few-trajs-noaugs \
+ il_train.bc.augs=None \
  tune_run_kwargs.num_samples=5 \
- tune_run_kwargs.resources_per_trial.gpu=0.3
+ tune_run_kwargs.resources_per_trial.gpu=0.1
