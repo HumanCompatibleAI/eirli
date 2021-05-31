2604625e2ba9d5a55c25037fe677a028ab64aa3c
Cynthia <cyn0531@hku.hk>
2021-05-31 15:21:49 +0800

    Fix the Path bug

--- src/il_representations/scripts/pretrain_n_adapt.py
+++ src/il_representations/scripts/pretrain_n_adapt.py
@@ -350,7 +350,10 @@ def run_end2end_exp(*, rep_ex_config, il_train_ex_config, il_test_ex_config,
         },
     )
     il_train_rv = run_single_exp(merged_il_train_config, log_dir, 'il_train')
-    il_policy_dir = os.path.join(il_train_rv['result']['model_path'].parent.absolute(),
+    il_train_model_path = il_train_rv['result']['model_path']
+    if not isinstance(il_train_model_path, Path):
+        il_train_model_path = Path(il_train_model_path)
+    il_policy_dir = os.path.join(il_train_model_path.parent.absolute(),
                                  'snapshot')
 
     # Run il test
