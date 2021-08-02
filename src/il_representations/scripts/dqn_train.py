72137be9296858c0b2e2cd6e0061a7adad2ac44a
Cynthia Chen <cyn0531@hku.hk>
2021-08-01 19:19:59 -0700

    Add initial training setup

--- src/il_representations/scripts/dqn_train.py
+++ src/il_representations/scripts/dqn_train.py
@@ -1,12 +1,20 @@
+#!/usr/bin/env python3
+"""Run offline DQN training, with trajectories loaded to the replay buffer."""
 import sacred
+import contextlib
 import logging
+import faulthandler
 import os
-import torch
+# readline import is black magic to stop PDB from segfaulting; do not remove it
+import readline  # noqa: F401
+import signal
+import torch as th
 import numpy as np
 from torch import nn
 from torch.optim.adam import Adam
 from sacred import Experiment, Ingredient
 from sacred.observers import FileStorageObserver
+from stable_baselines3.dqn import CnnPolicy
 
 from il_representations.algos.encoders import BaseEncoder
 from il_representations.algos.utils import set_global_seeds
@@ -15,6 +23,8 @@ import il_representations.envs.auto as auto_env
 from il_representations.envs.config import (env_cfg_ingredient,
                                             env_data_ingredient,
                                             venv_opts_ingredient)
+
+sacred.SETTINGS['CAPTURE_MODE'] = 'no'  # workaround for sacred issue#740
 dqn_ex = Experiment(
     'dqn',
     ingredients=[
@@ -40,11 +50,51 @@ def default_config():
         representation_dim=128,
         obs_encoder_cls_kwargs={}
     )
+    n_batches = 5000
+    augs = 'translate,rotate,gaussian_blur,color_jitter_ex'
+    save_every_n_batches = 50000
+    n_trajs = None
+    torch_num_threads = None
+
+
+@dqn_ex.capture
+def do_training_dqn():
+    policy = make_policy()
+    color_space = auto_env.load_color_space()
+    augmenter = augmenter_from_spec(bc['augs'], color_space)
+
+    # Build dataset in the format required by SB3's dqn training.
+
+    # Initialize DQN agent.
+
+    # Load data into DQN agent's memory.
+
+    # Call DQN training.
+
+    # Save policy.
 
 
 @dqn_ex.main
-def train():
-    pass
+def train(seed, torch_num_threads, dataset_configs, _config):
+    faulthandler.register(signal.SIGUSR1)
+    set_global_seeds(seed)
+    # python built-in logging
+    logging.basicConfig(level=logging.INFO)
+    log_dir = os.path.abspath(dqn_ex.observers[0].dir)
+
+    if torch_num_threads is not None:
+        th.set_num_threads(torch_num_threads)
+
+    with contextlib.closing(auto_env.load_vec_env()) as venv:
+        demo_webdatasets, combined_meta = auto_env.load_wds_datasets(
+            configs=dataset_configs)
+
+    final_path = do_training_dqn(
+                demo_webdatasets=demo_webdatasets,
+                venv_chans_first=venv,
+                out_dir=log_dir)
+
+    return {'model_path': os.path.abspath(final_path)}
 
 
 if __name__ == '__main__':
