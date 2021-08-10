ca872eead6c960e904b77185ecd573873505428b
Cynthia Chen <cyn0531@hku.hk>
2021-08-09 18:58:21 -0700

    interpret organized model files

--- src/il_representations/scripts/run_interpret.sh
+++ src/il_representations/scripts/run_interpret.sh
@@ -1,13 +1,23 @@
-model_dir=/scratch/cynthiachen/ilr-results/cluster-results/bc-repl-pretrain-procgen-dmc/models-dmc
+model_dir=/scratch/cynthiachen/ilr-results/joint-train-procgen-dmc-2020-06-24/interpret
 
 for model_path in $model_dir/* ; do
     model_filename=$(echo "${model_path%.*}")
     model_filename=$(echo $model_filename | rev | cut -d'/' -f 1 | rev)
-    agent=$(echo $model_filename | cut -d'-' -f 1)
-    action=$(echo $model_filename | cut -d'-' -f 2)
-    algo=$(echo $model_filename | cut -d'-' -f 3)
-    task_name=$agent-$action
+    # Check number of '-'s (Helpful for recognizing DMC or procgen env)
+    num_dashes=$(echo "${model_filename}" | awk -F"-" '{print NF-1}')
+    # echo $num_dashes
+    if [ $num_dashes -eq 1 ]
+    then
+	task_name=$(echo $model_filename | cut -d'-' -f 1)
+        algo=$(echo $model_filename | cut -d'-' -f 2)
+    else
+	agent=$(echo $model_filename | cut -d'-' -f 1)
+	action=$(echo $model_filename | cut -d'-' -f 2)
+        task_name=$agent-$action
+        algo=$(echo $model_filename | cut -d'-' -f 3)
+    fi
     echo $task_name, $algo
+
     CUDA_VISIBLE_DEVICES=$device python src/il_representations/scripts/interpret.py with \
         encoder_path=$model_path \
         save_video=True \
