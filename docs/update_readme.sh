e1c958eed3e393df4c968f25bfe9ad39ecf3f3e7
Sam Toyer <sam@qxcv.net>
2021-09-02 17:11:08 -0700

    Change update_readme.rst (untested)

--- docs/update_readme.sh
+++ docs/update_readme.sh
@@ -6,6 +6,7 @@
 
 set -euf -o pipefail
 
-pandoc -f markdown -t rst ../README.md -o source/README_former.rst --wrap=none
-# one of the figures is a link to a file on docs/src; we replace this
-sed -i -e 's-^.. figure:: docs/source/-.. figure:: -g' source/README_former.rst
+pandoc -f markdown -t rst --wrap=none '../README.md' -o 'source/README_former.rst'
+# One of the figures is a link to a file in `docs/source/`. We remove the
+# `docs/source/` so that the link is relative to the Sphinx document root.
+sed -i -e 's-^.. figure:: docs/source/-.. figure:: -g' 'source/README_former.rst'
